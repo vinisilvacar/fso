9102e166b91a4d9bd829a5ef91f88f63b041fe7c
Vinicius Carvalho <vinisilvacar@gmail.com>
2016-09-14 20:59:46 -0300

    Estrutura trabalho 03

---
+++ trabalho3/libprimo.h
@@ -0,0 +1,10 @@
+#ifndef LIBPRIMO_H
+#define LIBPRIMO_H
+
+//Funcao que gera um numero primoaleatorio de 32 bits.
+int gera_primo();
+
+//Funcao que testa um numero primo de 32 bits.
+int testa_primo(int primo);
+
+#endif
